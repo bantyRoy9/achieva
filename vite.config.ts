62728e963d82899d5a637b599744664593180163
Banti Kumar <mr.bantikumar9716@gmail.com>
2025-09-28 21:57:10 +0530

    fixed responsiveness

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // 👈 allows access via local IP
+    port: 3000, // optional, specify port
+  },
 })
